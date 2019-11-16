d31451f28ba50da2fac8167460bea7a32e9403bf
俊华 蔡 <1032031912@qq.com>
2019-11-16 11:20:33 +0800

    完成接口函数实现

--- COM_TXT_ClassLibrary/Class1.cs
+++ COM_TXT_ClassLibrary/Class1.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 //DLL 引用
 using System.Runtime.InteropServices;
+using System.IO;
 
 namespace COM_TXT_ClassLibrary
 {
@@ -19,8 +20,42 @@ namespace COM_TXT_ClassLibrary
         string Read_txt(string path);
     }
 
-    public class Class1
+    //2.实现接口
+    [ComVisible(true)]
+    [Guid("F821F9E9-EE2D-4A6B-B912-68D829E3AFFD")]
+    [ProgId("COM_TXT_ClassLibrary.Class1")]
+    public class Class1: IMyClass
     {
+        public void wirte_txt(string path, string content)
+        {
+            //创建文件流
+            FileStream fs = new FileStream(path, FileMode.Create);
+            //创建写入器
+            StreamWriter sw = new StreamWriter(fs);
+            //以文件流的形式写入数据
+            sw.Write(content);
+            //关闭写入器
+            sw.Close();
+            //关闭文件流
+            fs.Close();
+        }
 
+        public string Read_txt(string path)
+        {
+            //读取到的内容
+            string str;
+            //创建文件流
+            FileStream fs = new FileStream(path, FileMode.Open);
+            //创建操作器
+            StreamReader sr = new StreamReader(fs);
+            //文件流的方式读取数据
+            str = sr.ReadToEnd();
+            //关闭写入器
+            sr.Close();
+            //关闭文件流
+            fs.Close();
+            //返回内容
+            return str;
+        }
     }
 }
